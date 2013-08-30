cba61d393e53159df01ef97df277d71e4b13cb6a
Josh Wilsdon <jwilsdon@joyent.com>
2013-08-30 16:34:41 -0700

    HEAD-1804 switch to new layout for /opt/smartdc/sdc-boot and bump sdc-scripts

--- sdc-boot/setup.sh
+++ sdc-boot/setup.sh
@@ -11,7 +11,7 @@ PATH=/opt/local/bin:/opt/local/sbin:/usr/bin:/usr/sbin
 CONFIG_AGENT_LOCAL_MANIFESTS_DIRS=/opt/smartdc/$role
 
 # Include common utility functions (then run the boilerplate)
-source /opt/smartdc/sdc-boot/scripts/util.sh
+source /opt/smartdc/sdc-boot/lib/util.sh
 sdc_common_setup
 
 role=${zone_role}

f13d737fa4aeef674981cb116d9ad02e3f543670
Josh Wilsdon <jwilsdon@joyent.com>
2013-09-11 16:25:05 -0700

    HEAD-1804 fix sapi upgrade / bootstrapping problems.

--- boot/setup.sh
+++ boot/setup.sh
@@ -9,7 +9,7 @@ set -o xtrace
 
 PATH=/opt/local/bin:/opt/local/sbin:/usr/bin:/usr/sbin
 
-CONFIG_AGENT_LOCAL_MANIFESTS_DIRS=/opt/smartdc/$role
+CONFIG_AGENT_LOCAL_MANIFESTS_DIRS=/opt/smartdc/sapi
 
 # Include common utility functions (then run the boilerplate)
 source /opt/smartdc/boot/lib/util.sh
@@ -31,19 +31,24 @@ echo "export PATH=\$PATH:/opt/smartdc/sapi/build/node/bin:/opt/smartdc/sapi/node
 
 # bootstrap the config file once only.
 
-# During setup/bootstrapping, we do not expect binder to be available, and
-# reply on the pre-allocated IPs.  We grab all the config from the usbkey_config
-# key in metadata which is assumed to have a copy of /usbkey/config for us.
+SAPI_MODE=$(mdata-get SAPI_MODE)
+[[ -n ${SAPI_MODE} ]] || SAPI_MODE="proto"
 
-/usr/sbin/mdata-get usbkey_config > /var/tmp/usbkey.config
-if [[ $? -ne 0 ]]; then
-    echo "Unable to find usbkey/config in SAPI zone." >&2
-    exit 1
-fi
+if [[ ${SAPI_MODE} == "proto" ]]; then
 
-eval $(
-. /var/tmp/usbkey.config
-cat <<EOF
+    # During setup/bootstrapping, we do not expect binder to be available, and
+    # reply on the pre-allocated IPs.  We grab all the config from the usbkey_config
+    # key in metadata which is assumed to have a copy of /usbkey/config for us.
+
+    /usr/sbin/mdata-get usbkey_config > /var/tmp/usbkey.config
+    if [[ $? -ne 0 ]]; then
+        echo "Unable to find usbkey/config in SAPI zone." >&2
+        exit 1
+    fi
+
+    eval $(
+    . /var/tmp/usbkey.config
+    cat <<EOF
 DATACENTER_NAME=${datacenter_name}
 IMGAPI_ADMIN_IPS=${imgapi_admin_ips}
 MORAY_ADMIN_IPS=${moray_admin_ips}
@@ -58,24 +63,24 @@ UFDS_LDAP_ROOT_PW=${ufds_ldap_root_pw}
 WFAPI_HTTP_ADMIN_USER=${workflow_http_admin_user}
 WFAPI_HTTP_ADMIN_PW=${workflow_http_admin_pw}
 EOF
-)
+    )
 
-IMGAPI_URL=http://$(echo "${IMGAPI_ADMIN_IPS}" | cut -d',' -f1)
-MORAY_HOST=$(echo "${MORAY_ADMIN_IPS}" | cut -d ',' -f1)
-NAPI_URL=http://$(echo "${NAPI_ADMIN_IPS}" | cut -d',' -f1)
-UFDS_URL=ldaps://$(echo "${UFDS_ADMIN_IPS}" | cut -d ',' -f1)
-CNAPI_URL=http://$(echo "${CNAPI_ADMIN_IPS}" | cut -d',' -f1)
-VMAPI_URL=http://$(echo "${VMAPI_ADMIN_IPS}" | cut -d',' -f1)
-WFAPI_URL=http://$(echo "${WORKFLOW_ADMIN_IPS}" | cut -d',' -f1)
+    IMGAPI_URL=http://$(echo "${IMGAPI_ADMIN_IPS}" | cut -d',' -f1)
+    MORAY_HOST=$(echo "${MORAY_ADMIN_IPS}" | cut -d ',' -f1)
+    NAPI_URL=http://$(echo "${NAPI_ADMIN_IPS}" | cut -d',' -f1)
+    UFDS_URL=ldaps://$(echo "${UFDS_ADMIN_IPS}" | cut -d ',' -f1)
+    CNAPI_URL=http://$(echo "${CNAPI_ADMIN_IPS}" | cut -d',' -f1)
+    VMAPI_URL=http://$(echo "${VMAPI_ADMIN_IPS}" | cut -d',' -f1)
+    WFAPI_URL=http://$(echo "${WORKFLOW_ADMIN_IPS}" | cut -d',' -f1)
 
-echo "Creating SAPI config file"
-mkdir -p /opt/smartdc/sapi/etc
+    echo "Creating SAPI config file"
+    mkdir -p /opt/smartdc/sapi/etc
 
-# This config file is used during setup to bootstrap SAPI. With the exception
-# that it requires IP addresses instead of DNS names (as binder is not expected
-# to be setup yet), it should be kept broadly in sync with the template at:
-# $USB_HEADNODE_ROOT/config/sapi/manifests/services/sapi/sapi/template
-cat > /opt/smartdc/sapi/etc/config.json <<HERE
+    # This config file is used during setup to bootstrap SAPI. With the exception
+    # that it requires IP addresses instead of DNS names (as binder is not expected
+    # to be setup yet), it should be kept broadly in sync with the template at:
+    # $USB_HEADNODE_ROOT/config/sapi/manifests/services/sapi/sapi/template
+    cat > /opt/smartdc/sapi/etc/config.json <<HERE
 {
   "log_options": {
     "name": "sapi",
@@ -114,6 +119,8 @@ cat > /opt/smartdc/sapi/etc/config.json <<HERE
   }
 }
 HERE
+
+fi  # SAPI_MODE == proto
 
 echo "Adding log rotation"
 logadm -w sapi -C 48 -s 100m -p 1h \

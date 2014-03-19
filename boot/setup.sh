37ba0c6ad5507b733067351011eb0659de1b2c44
Trent Mick <trentm@gmail.com>
2014-03-19 15:39:36 -0700

    SAPI-211: SAPI-167 changes broke setup of a second sapi zone

--- boot/setup.sh
+++ boot/setup.sh
@@ -8,40 +8,31 @@ export PS4='[\D{%FT%TZ}] ${BASH_SOURCE}:${LINENO}: ${FUNCNAME[0]:+${FUNCNAME[0]}
 set -o xtrace
 
 PATH=/opt/local/bin:/opt/local/sbin:/usr/bin:/usr/sbin
-
-CONFIG_AGENT_LOCAL_MANIFESTS_DIRS=/opt/smartdc/sapi
-
-# Include common utility functions (then run the boilerplate)
-source /opt/smartdc/boot/lib/util.sh
-sdc_common_setup
-
-role=${zone_role}
+role=sapi
 
 # Cookie to identify this as a SmartDC zone and its role
 mkdir -p /var/smartdc/sapi
-
-# Install SAPI
-mkdir -p /opt/smartdc/sapi
-chown -R nobody:nobody /opt/smartdc/sapi
 
 # Add build/node/bin and node_modules/.bin to PATH
 echo "" >>/root/.profile
 echo "export PATH=\$PATH:/opt/smartdc/sapi/build/node/bin:/opt/smartdc/sapi/node_modules/.bin" >>/root/.profile
 
-# bootstrap the config file once only.
 
-SAPI_MODE=$(mdata-get SAPI_MODE)
-[[ -n ${SAPI_MODE} ]] || SAPI_MODE="proto"
+# Include common utility functions (then run the boilerplate).
+source /opt/smartdc/boot/lib/util.sh
+CONFIG_AGENT_LOCAL_MANIFESTS_DIRS=/opt/smartdc/sapi
+sdc_common_setup
 
-if [[ ${SAPI_MODE} == "proto" ]]; then
-
+SAPI_PROTO_MODE=$(mdata-get SAPI_PROTO_MODE || true)
+if [[ "${SAPI_PROTO_MODE}" == "true" ]]; then
     # During setup/bootstrapping, we do not expect binder to be available, and
-    # reply on the pre-allocated IPs.  We grab all the config from the usbkey_config
-    # key in metadata which is assumed to have a copy of /usbkey/config for us.
+    # rely on the pre-allocated IPs. We grab all the config from the
+    # usbkey_config key in metadata which is assumed to have a copy of
+    # /usbkey/config for us.
 
     /usr/sbin/mdata-get usbkey_config > /var/tmp/usbkey.config
     if [[ $? -ne 0 ]]; then
-        echo "Unable to find usbkey/config in SAPI zone." >&2
+        echo "error: Unable to find usbkey/config in SAPI zone." >&2
         exit 1
     fi
 
@@ -63,20 +54,18 @@ EOF
     CNAPI_URL=http://$(echo "${CNAPI_ADMIN_IPS}" | cut -d',' -f1)
     VMAPI_URL=http://$(echo "${VMAPI_ADMIN_IPS}" | cut -d',' -f1)
 
-    echo "Creating SAPI config file"
+    # This config file is used during setup to bootstrap SAPI. With the
+    # exception that it requires IP addresses instead of DNS names (as binder is
+    # not expected to be setup yet), it should be kept broadly in sync with the
+    # template at:
+    #   sapi.git/sapi_manifests/sapi/template
     mkdir -p /opt/smartdc/sapi/etc
-
-    # This config file is used during setup to bootstrap SAPI. With the exception
-    # that it requires IP addresses instead of DNS names (as binder is not expected
-    # to be setup yet), it should be kept broadly in sync with the template at:
-    # $USB_HEADNODE_ROOT/config/sapi/manifests/services/sapi/sapi/template
     cat > /opt/smartdc/sapi/etc/config.json <<HERE
 {
   "log_options": {
     "name": "sapi",
     "level": "debug"
   },
-  "mode": "proto",
   "datacenter_name": "$DATACENTER_NAME",
   "moray": {
     "host": "$MORAY_HOST",
@@ -97,7 +86,7 @@ EOF
 }
 HERE
 
-fi  # SAPI_MODE == proto
+fi
 
 echo "Adding log rotation"
 sdc_log_rotation_add amon-agent /var/svc/log/*amon-agent*.log 1g

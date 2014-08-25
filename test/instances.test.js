d5dc5339ee19239d7abc55e51f6f8d2e749dfe90
Nate Fitch <nate.fitch@joyent.com>
2014-08-25 17:53:18 +0000

    TOOLS-607: I for one, welcome my new open source license

--- test/instances.test.js
+++ test/instances.test.js
@@ -1,6 +1,14 @@
 /*
- * Copyright (c) 2013, Joyent, Inc. All rights reserved.
- *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+/*
+ * Copyright (c) 2014, Joyent, Inc.
+ */
+
+/*
  * test/instances.test.js: test /instances endpoints
  */
 //
@@ -469,14 +477,15 @@ test('put/get/del agent instance', function (t) {
 			});
 		},
 		function (cb) {
-			client.get(URI + '?type=agent', function (err, _, res, obj) {
+			function onRes(err, _, res, obj) {
 				t.ifError(err);
 				t.equal(res.statusCode, 200);
 
 				checkInstanceInArray(obj);
 
 				cb(null);
-			});
+			}
+			client.get(URI + '?type=agent', onRes);
 		},
 		function (cb) {
 			var uri = sprintf('/instances/%s/payload', inst.uuid);
